2fa602ad6eeda5b5cde7bd148456fb3bca14ada9
Mattsz <6matheuslima@gmail.com>
2022-06-08 17:25:14 +0000

    Adicionou bootstrap, criamos os templates e a passagem de dados para html

--- app.py
+++ app.py
@@ -1,11 +1,19 @@
 # Ao abrir o GitPod, execute:
 # pip install -r requirements.txt
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
+clientes = [
+    {'nome': 'Fulano', 'email': 'fulano@gmail.com', 'telefone': '1234567'},
+    {'nome': 'Ciclano', 'email': 'Ciclano@gmail.com', 'telefone': '17988208223'}
+]
 
 @app.route('/')
-def hello():
-    return 'Hello world!!'
+def index():
+    return render_template('index.html', clientes=clientes)
 
-app.run()
+@app.route('/create')
+def create():
+    return render_template('create.html')
+
+app.run(debug=True)
